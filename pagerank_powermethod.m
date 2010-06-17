1862ded739709aa099e73dfea6298ae67cb252cc
Nick Davis <nick@nickdavis.name>
2010-06-17 12:04:20 -0500

    p -> gamma and additional cleanup

--- pagerank_powermethod.m
+++ pagerank_powermethod.m
@@ -1,17 +1,15 @@
-function r = pagerank_powermethod(names,G,p,resultsbase,capturedata)
-% PAGERANK  Google's PageRank
-% [header100,data100]=parsefile('Titers.Matrix.top100.txt');
-% pagerank_powermethod(header100,data100,.85,'results');
+function r = pagerank_powermethod(names, G, gamma, resultsbase, capturedata)
+% SNPRank - SNP ranking algorithm
+% [header100,data100] = parsefile('Titers.Matrix.top100.txt');
+% pagerank_powermethod(header100, data100);
 
-% Uses URLs (SNPs) U and adjacency matrix G produced by parsefile,
-% together with a damping factory p, (default is .85), to compute and plot
-% a bar graph of page rank, and print the dominant URLs (SNPs) in page rank order.
-% x = pagerank(U,G,p) returns the page ranks instead of printing.
-% See also SURFER, SPY.
+% Uses names (SNPs) and adjacency matrix G produced by parsefile,
+% together with a damping factory gamma, (default is .85), to compute and plot
+% a bar graph of page rank, and print the dominant SNPs in rank order.
 
 % Set defaults for optional params
 if nargin < 5
-    p = .85; 
+    gamma = .85; 
     resultsbase = 'results';
     capturedata = false;
 end
@@ -20,30 +18,27 @@ end
 Gdiag = diag(G);
 % Sum of diagonal elements
 Gtrace = sum(Gdiag);
-% Creates a new matrix of 0s and sets Gs diagonal as the diagonal, 
-% and subtracts from G.  Thus the result is G minus its diagonal.
-Gnodiag = G - diag(Gdiag);   % sometimes not necessary
 
 % colsum = out-degree, rowsum = in-degree
 [n,n] = size(G);
-colsum = sum(G, 1);  % 1xn vector of column sums
-rowsum = sum(G, 2);  % nx1 vector of row sums
+colsum = sum(G, 1);  % 1 x n (row) vector of column sums
+rowsum = sum(G, 2);  % n x 1 (column) vector of row sums
 
-% Scale G column sums to be 1 (or 0 where there are no out links).
-colsum_idx = find(colsum ~= 0);  % indices of colsum vector that are not zero 
+% indices of colsum vector that are non-zero, zero 
+colsum_nzidx = find(colsum ~= 0);  
+colsum_zidx = find(colsum == 0);  
 
-% nxn sparse matrix with values 1/colsum for nonzero elements of colsum (indices
-% given by k).  Other elements are zero.
-D = sparse(colsum_idx, colsum_idx, Gtrace ./ colsum(colsum_idx), n, n);  
+% n x n sparse matrix with values 1/colsum for nonzero elements of colsum 
+% indices given by colsum_nzidx).  Other elements are zero.
+D = sparse(colsum_nzidx, colsum_nzidx, Gtrace ./ colsum(colsum_nzidx), n, n);  
 
 % compute initial T, Markov chain transition matrix
-z_T(colsum_idx) = (1-p);
-T = (p * G * D + Gdiag * z_T) / Gtrace;
-
+z_T(colsum_nzidx) = 1 - gamma;
+T = (gamma * G * D + Gdiag * z_T) / Gtrace;
 
 % iterate power method
-unit = ones(n,1); % column vector of 1s
-r = unit/n; % initial arbitrary vector
+unit = ones(n, 1); % column vector of 1s
+r = unit / n; % initial arbitrary vector
 for i=1:5
     r = T * r;
     lambda = sum(r);  % temporary eigenvalue
@@ -65,25 +60,25 @@ if capturedata
         % need to replace _ with \_ so Matlab doesn't subscript the labels
         text(topindices(i), topsnps(i),strrep(names(topindices(i)),'_', '\_'), 'Rotation', 90)
     end
-    saveas(h, [resultsbase '-bar' num2str(p) '.eps'], 'psc2');
+    saveas(h, [resultsbase '-bar' num2str(gamma) '.eps'], 'psc2');
 
     % Scatter plot of SNPRank score vs. node degree
     scorevdeg = scatter(rowsum, r);
     xlabel('degree');
     ylabel('SNPRank score');
-    title([resultsbase ' score vs. degree, p = ' num2str(p)])
-    saveas(scorevdeg, [resultsbase '-degree-scatter' num2str(p) '.eps'], 'psc2')
+    title([resultsbase ' score vs. degree, gamma = ' num2str(gamma)])
+    saveas(scorevdeg, [resultsbase '-degree-scatter' num2str(gamma) '.eps'], 'psc2')
 
     % Scatter plot of SNPRank score vs. information gain (IG)
     scorevig = scatter(Gdiag, r);
     xlabel('Information gain (IG)');
     ylabel('SNPRank score');
-    title([resultsbase ' score vs. IG, p = ' num2str(p)])
-    saveas(scorevig, [resultsbase '-IG-scatter' num2str(p) '.eps'], 'psc2')
+    title([resultsbase ' score vs. IG, gamma = ' num2str(gamma)])
+    saveas(scorevig, [resultsbase '-IG-scatter' num2str(gamma) '.eps'], 'psc2')
 
 
     % Print SNPs in SNPrank order.
-    fid = fopen([resultsbase '-' num2str(p) '.txt'], 'w');
+    fid = fopen([resultsbase '-' num2str(gamma) '.txt'], 'w');
     if nargout < 1
        [ignore,q] = sort(-r);
        fprintf(fid,'SNP \t snp-rank \t in \t out\n');
