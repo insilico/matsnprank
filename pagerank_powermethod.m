1cc0da4c7c428d47667ba4d8a31b2cb003871647
Nick Davis <nick@nickdavis.name>
2010-06-21 13:29:13 -0500

    Fewer params and general cleanup

--- pagerank_powermethod.m
+++ pagerank_powermethod.m
@@ -1,19 +1,28 @@
-function r = pagerank_powermethod(names, G, gamma, resultsbase, capturedata)
+function r = pagerank_powermethod(datafile, gamma, capturedata)
 % SNPRank - SNP ranking algorithm
-% [header100,data100] = parsefile('Titers.Matrix.top100.txt');
-% pagerank_powermethod(header100, data100);
 
-% Uses names (SNPs) and adjacency matrix G produced by parsefile,
-% together with a damping factory gamma, (default is .85), to compute and plot
-% a bar graph of page rank, and print the dominant SNPs in rank order.
-% Returns the final SNPRank score vector r
+% Uses SNP names (SNPs) and adjacency matrix G produced by parsefile,
+% together with a damping factory gamma, (default is .85), to compute 
+% SNPRank scores.
+% Returns the final SNPRank score vector r, in same order as original 
+% data matrix/SNP names
+%
+% Usage:  pagerank_powermethod('gain-matrix.txt');
+% Authors:  Brett McKinney and Nick Davis
+% Email:  brett.mckinney@gmail.com, nick@nickdavis.name
 
 % Set defaults for optional params
-if nargin < 5
-    gamma = .85; 
-    resultsbase = 'results';
+if nargin < 3
+    gamma = .85;
     capturedata = false;
 end
+
+% Use file prefix (everything preceding .ext) for saved data files
+namesplit = regexp(datafile, '\....$', 'split', 'stringanchors');
+resultsbase = namesplit(1);
+
+% Parse data matrix file, store headers (SNPs) and data as vars
+[SNPs, G] = parsefile(datafile);
 
 % G diagonal is information gain  
 Gdiag = diag(G);
@@ -23,6 +32,7 @@ Gtrace = sum(Gdiag);
 % colsum = out-degree, rowsum = in-degree (in undirected graphs
 % out-degrees = in-degree)
 [n,n] = size(G);
+
 % 1 x n (row) vector of column sums (d_j in Eqs. 4, 5 from SNPRank paper)
 colsum = sum(G, 1);  
 % n x 1 (column) vector of row sums
@@ -37,9 +47,11 @@ D = sparse(colsum_nzidx, colsum_nzidx, 1 ./ colsum(colsum_nzidx), n, n);
 
 % initialize second term multiplier as a column vector of 1s
 T_nz = ones(1, n);
+
 % non-zero elements of colsum/d_j have (1 - gamma) in the numerator of the 
 % second term (Eq. 5 from SNPRank paper)
 T_nz(colsum_nzidx) = 1 - gamma;
+
 % compute initial T, Markov chain transition matrix
 % first term (gamma * G * D) is zero when d_j = 0
 T = (gamma * G * D) + (Gdiag * T_nz) / Gtrace;
@@ -53,54 +65,55 @@ for i=1:5
     r = r / lambda;     % Normalize eivenvector so that sum(r) == 1.
 end
 
-% Only show figures and save data to file if capturedata is set
+% Bar graph of SNPRank, with labels for top SNPs
+figure(1)
+h = bar(r);
+title('SNPRank scores')
+% Capture top 10 SNPs by SNPRank, label vertically on bar graph
+[sortedranks, snpindices] = sort(r, 'descend');
+topsnps = sortedranks(1:10);
+topindices = snpindices(1:10);
+for i=1:length(topindices)
+    % need to replace _ with \_ so Matlab doesn't subscript the labels
+    text(topindices(i), topsnps(i),strrep(SNPs(topindices(i)),'_', '\_'), 'Rotation', 90)
+end
+% Only save figures and data to file if capturedata is set
 if capturedata
-    % Bar graph of page rank, with labels for top SNPs
-    shg
-    h = bar(r);
-    title('SNP Rank')
-
-    [sortedranks, snpindices] = sort(r, 'descend');
-    % Capture top 10 SNPs by SNPRank
-    topsnps = sortedranks(1:10);
-    topindices = snpindices(1:10);
-    for i=1:length(topindices)
-        % need to replace _ with \_ so Matlab doesn't subscript the labels
-        text(topindices(i), topsnps(i),strrep(names(topindices(i)),'_', '\_'), 'Rotation', 90)
-    end
     saveas(h, [resultsbase '-bar' num2str(gamma) '.eps'], 'psc2');
+end
 
-    % Scatter plot of SNPRank score vs. node degree
-    scorevdeg = scatter(rowsum, r);
-    xlabel('degree');
-    ylabel('SNPRank score');
-    title([resultsbase ' score vs. degree, gamma = ' num2str(gamma)])
+% Scatter plot of SNPRank score vs. node degree
+figure(2)
+scorevdeg = scatter(rowsum, r);
+xlabel('degree');
+ylabel('SNPRank score');
+title([resultsbase ' score vs. degree, gamma = ' num2str(gamma)])
+if capturedata
     saveas(scorevdeg, [resultsbase '-degree-scatter' num2str(gamma) '.eps'], 'psc2')
+end
 
-    % Scatter plot of SNPRank score vs. information gain (IG)
-    scorevig = scatter(Gdiag, r);
-    xlabel('Information gain (IG)');
-    ylabel('SNPRank score');
-    title([resultsbase ' score vs. IG, gamma = ' num2str(gamma)])
+% Scatter plot of SNPRank score vs. information gain (IG)
+figure(3)
+scorevig = scatter(Gdiag, r);
+xlabel('Information gain (IG)');
+ylabel('SNPRank score');
+title([resultsbase ' score vs. IG, gamma = ' num2str(gamma)])
+if capturedata
     saveas(scorevig, [resultsbase '-IG-scatter' num2str(gamma) '.eps'], 'psc2')
+end
 
-
-    % Print SNPs in SNPrank order.
+% Print SNPs in SNPrank order
+fid = 1; % unless capturedata set, only print to console
+if capturedata
     fid = fopen([resultsbase '-' num2str(gamma) '.txt'], 'w');
-    if nargout < 1
-       [ignore,q] = sort(-r);
-       fprintf(fid,'SNP \t snp-rank \t in \t out\n');
-       fprintf('index \t snp-rank \t in \t out \t SNP \t IG (main effect)')
-       k = 1;
-       fprintf('n: %i', n)
-       while (k <= n)
-          j = q(k);
-          fprintf(' %3.0f \t %8.4f \t %4.0f \t %4.0f \t %s \t %8.4f\n', ...
-             j,r(j),rowsum(j),colsum(j),names{j},G(j,j))
-         fprintf(fid,'%s \t %8.4f \t %4.0f \t %4.0f \t \n', ...
-             names{j},r(j),rowsum(j),colsum(j));
-          k = k + 1;
-       end
-    end
+end
+[~,q] = sort(-r);
+fprintf(fid,'SNP \t snp-rank \t IG (main effect) \t in \t out\n');
+for k = 1:n
+  j = q(k);
+  fprintf(fid,'%s \t %8.4f \t %8.4f \t %4.0f \t %4.0f \t \n', ...
+     SNPs{j}, r(j), G(j,j), rowsum(j), colsum(j));
+end
+if capturedata
     fclose(fid);
 end
